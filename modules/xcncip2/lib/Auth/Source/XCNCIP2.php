7cd6ea7eb4ef5d5716d2aa9559307a6def09a83e
jirislav <mail@jkozlovsky.cz>
2015-05-02 16:53:56 +0200

    Implemented parsing user info from NCIP message

--- modules/xcncip2/lib/Auth/Source/XCNCIP2.php
+++ modules/xcncip2/lib/Auth/Source/XCNCIP2.php
@@ -15,17 +15,43 @@ class sspmod_xcncip2_Auth_Source_XCNCIP2 extends sspmod_core_Auth_UserPassBase {
 		$requestBody = $this->getLookupUserRequest($username, $password);
 		$response = $this->doRequest($requestBody);
 		$id = $response->xpath(
-			'ns1:LookupUserResponse/ns1:UserId/ns1:UserIdentifierValue'
-		);
+				'ns1:LookupUserResponse/ns1:UserId/ns1:UserIdentifierValue'
+				);
 		if (!empty($id)) {
+
+			$userId = (String) $response->xpath(
+					'ns1:LookupUserResponse/ns1:UserId/ns1:UserIdentifierValue')[0];
+
+			$electronicAddresses = $response->xpath(
+					'ns1:LookupUserResponse/ns1:UserOptionalFields/ns1:UserAddressInformation/ns1:ElectronicAddress'
+					);
+			foreach ($electronicAddresses as $recent) {
+				if (strpos((String) $recent->xpath('ns1:ElectronicAddressType')[0], 'mail') !== FALSE) {
+					$mail = (String) $recent->xpath('ns1:ElectronicAddressData')[0];
+					break;
+				}
+			}
+
+			$firstname = (String) $response->xpath(
+					'ns1:LookupUserResponse/ns1:UserOptionalFields/ns1:NameInformation/' .
+					'ns1:PersonalNameInformation/ns1:StructuredPersonalUserName/ns1:GivenName')[0];
+
+			$lastname = (String) $response->xpath(
+					'ns1:LookupUserResponse/ns1:UserOptionalFields/ns1:NameInformation/' .
+					'ns1:PersonalNameInformation/ns1:StructuredPersonalUserName/ns1:Surname')[0];
 			return array(
-				'uid' => array($username),
-				'eduPersonScopedAffiliation' => $this->eduPersonScopedAffiliation,
-			);
+					'userId' => array($username),
+					'eduPersonScopedAffiliation' => $this->eduPersonScopedAffiliation,
+					'eduPersonPrincipalName' => array($userId),
+					'mail' => array($mail),
+					'givenName' => array($firstname),
+					'sn' => array($lastname),
+					'pw' => array($password),
+				    );
 		} else {
 			throw new SimpleSAML_Error_Error('WRONGUSERPASS');
 		}
-		
+
 	}
 
 	protected function doRequest($body) {
@@ -33,11 +59,10 @@ class sspmod_xcncip2_Auth_Source_XCNCIP2 extends sspmod_core_Auth_UserPassBase {
 		curl_setopt($req, CURLOPT_POST, 1);
 		curl_setopt($req, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($req, CURLOPT_HTTPHEADER, array(
-			'Content-type: application/xml; charset=utf-8',
-		));
+					'Content-type: application/xml; charset=utf-8',
+					));
 		curl_setopt($req, CURLOPT_POSTFIELDS, $body);
 		$response = curl_exec($req);
-		print "$body";
 		$result = simplexml_load_string($response);
 		if (is_a($result, 'SimpleXMLElement')) {
 			$result->registerXPathNamespace('ns1', 'http://www.niso.org/2008/ncip');
@@ -52,30 +77,32 @@ class sspmod_xcncip2_Auth_Source_XCNCIP2 extends sspmod_core_Auth_UserPassBase {
 			'<ns1:NCIPMessage xmlns:ns1="http://www.niso.org/2008/ncip" ' .
 			'ns1:version="http://www.niso.org/schemas/ncip/v2_0/imp1/' .
 			'xsd/ncip_v2_0.xsd">' .
-				'<ns1:LookupUser>' .
-					'<ns1:AuthenticationInput>' .
-						'<ns1:AuthenticationInputData>' .
-							htmlspecialchars($username) .
-						'</ns1:AuthenticationInputData>' .
-						'<ns1:AuthenticationDataFormatType>' .
-							'text/plain' .
-						'</ns1:AuthenticationDataFormatType>' .
-						'<ns1:AuthenticationInputType>' .
-							'User Id' .
-						'</ns1:AuthenticationInputType>' .
-					'</ns1:AuthenticationInput>' .
-					'<ns1:AuthenticationInput>' .
-						'<ns1:AuthenticationInputData>' .
-							htmlspecialchars($password) .
-						'</ns1:AuthenticationInputData>' .
-						'<ns1:AuthenticationDataFormatType>' .
-							'text/plain' .
-						'</ns1:AuthenticationDataFormatType>' .
-						'<ns1:AuthenticationInputType>' .
-							'Password' .
-						'</ns1:AuthenticationInputType>' .
-					'</ns1:AuthenticationInput>' .
-				'</ns1:LookupUser>' .
+			'<ns1:LookupUser>' .
+			'<ns1:AuthenticationInput>' .
+			'<ns1:AuthenticationInputData>' .
+			htmlspecialchars($username) .
+			'</ns1:AuthenticationInputData>' .
+			'<ns1:AuthenticationDataFormatType>' .
+			'text/plain' .
+			'</ns1:AuthenticationDataFormatType>' .
+			'<ns1:AuthenticationInputType>' .
+			'User Id' .
+			'</ns1:AuthenticationInputType>' .
+			'</ns1:AuthenticationInput>' .
+			'<ns1:AuthenticationInput>' .
+			'<ns1:AuthenticationInputData>' .
+			htmlspecialchars($password) .
+			'</ns1:AuthenticationInputData>' .
+			'<ns1:AuthenticationDataFormatType>' .
+			'text/plain' .
+			'</ns1:AuthenticationDataFormatType>' .
+			'<ns1:AuthenticationInputType>' .
+			'Password' .
+			'</ns1:AuthenticationInputType>' .
+			'</ns1:AuthenticationInput>' .
+			'<ns1:UserElementType ns1:Scheme="http://www.niso.org/ncip/v1_0/schemes/userelementtype/userelementtype.scm">Name Information</ns1:UserElementType>' .
+			'<ns1:UserElementType ns1:Scheme="http://www.niso.org/ncip/v1_0/schemes/userelementtype/userelementtype.scm">User Address Information</ns1:UserElementType>' .
+			'</ns1:LookupUser>' .
 			'</ns1:NCIPMessage>';
 	}
 
